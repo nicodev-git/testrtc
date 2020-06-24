21a99e6c164c71158307115586bc5989a709eab4
Philipp Hancke <philipp.hancke@vonage.com>
2020-06-24 15:20:40 +0200

    initial commit

---
+++ src/main.js
@@ -0,0 +1,91 @@
+const uuid = require('uuid');
+const { connect } = require('twilio-video');
+
+async function fetchToken(user, roomName) {
+    const res = await fetch('/token?identity=' + user + '&roomName=' + roomName, {
+        method: 'GET',
+    });
+    return res.text();
+}
+
+const user = uuid.v4();
+const roomName = window.location.pathname.substr(1);
+fetchToken(user, roomName)
+.then(async (token) => {
+    console.log('token', token);
+    const options = {
+        audio: true,
+        video: {width: 1280},
+        preferredVideoCodecs: [
+            { codec: 'VP8', simulcast: true }
+        ]
+    };
+
+    const room = await connect(token, options);
+    window.addEventListener('beforeunload', () => room.disconnect());
+	console.log(`Successfully joined a Room: ${room}`);
+
+    // Display local video. Docs suck.
+    const localVideoContainer = document.getElementById('localVideoContainer');
+    room.localParticipant.videoTracks.forEach((track) => {
+        localVideoContainer.appendChild(track.track.attach());
+    });
+
+    // https://www.twilio.com/docs/video/javascript-getting-started#display-a-remote-participants-video
+    room.on('participantConnected', participant => {
+		console.log(`Participant "${participant.identity}" connected`);
+
+        const container = document.createElement('div');
+        container.id = participant.sid;
+        document.getElementById('remotes').appendChild(container);
+		participant.tracks.forEach(publication => {
+			if (publication.isSubscribed) {
+				const track = publication.track;
+				container.appendChild(track.attach());
+			}
+            publication.on('unsubscribed', () => {
+                if (publication.track) {
+                    const mediaElements = publication.track.detach();
+                    mediaElements.forEach(mediaElement => mediaElement.remove());
+                }
+            });
+		});
+
+		participant.on('trackSubscribed', track => {
+            container.appendChild(track.attach());
+		});
+	});
+	room.participants.forEach(participant => {
+        const container = document.createElement('div');
+        container.id = participant.sid;
+        document.getElementById('remotes').appendChild(container);
+		participant.tracks.forEach(publication => {
+			if (publication.track) {
+				const track = publication.track;
+				container.appendChild(track.attach());
+			}
+            publication.on('unsubscribed', () => {
+                if (publication.track) {
+                    const mediaElements = publication.track.detach();
+                    mediaElements.forEach(mediaElement => mediaElement.remove());
+                }
+            });
+		});
+
+		participant.on('trackSubscribed', track => {
+            container.appendChild(track.attach());
+        });
+    });
+    room.on('participantDisconnected', participant => {
+		console.log(`Participant disconnected: ${participant.identity}`);
+		participant.tracks.forEach(publication => {
+            console.log('bye', publication.track);
+            if (publication.track) {
+                const mediaElements = publication.track.detach();
+                mediaElements.forEach(mediaElement => mediaElement.remove());
+            }
+        });
+        document.getElementById(participant.sid).remove();
+	});
+
+});
