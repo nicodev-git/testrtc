7ce87682f35b9521feedf88b0a53d130114ff67e
nicodev-git <schaefernicker@gmail.com>
2021-01-12 19:56:47 +0200

    add function to refresh token

---
+++ functions/refresh-webex-token/refresh-webex-token.js
@@ -0,0 +1,13 @@
+const { save } = require("./redis-connection");
+
+// this will be called from CRON
+exports.handler = async () => {
+  const { token } = "TBD-refresh-token";
+  console.log(`refresh-webex-token:handler`);
+  await save(token);
+
+  return {
+    statusCode: 200,
+    body: "",
+  };
+};
