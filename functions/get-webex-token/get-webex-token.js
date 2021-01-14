25bb0d4a0ea826192e58b6633f4d1940d5ea9b48
nicodev-git <schaefernicker@gmail.com>
2021-01-15 00:23:00 +0530

    Trying webex oauth 2

--- functions/get-webex-token/get-webex-token.js
+++ functions/get-webex-token/get-webex-token.js
@@ -1,6 +1,7 @@
 const { get } = require("../save-webex-token/redis-connection");
 var Webex = require('webex');
 const assert = require(`assert`);
+const config = require('./server-config')
 
 exports.handler = async (event) => {
   //const token = await get();
@@ -12,7 +13,7 @@ exports.handler = async (event) => {
   var webex = Webex.init({
     config: {
       credentials: {
-        authorizationString: 'https://webexapis.com/v1/authorize?client_id=C6139709e0ad95b4d3a6027696af489a2047ed63de929939756a6ce6d8fa5db3c&response_type=code&redirect_uri=https%3A%2F%2Ftwilio-playground.netlify.app%2Fwebex%2Foauth%2Fredirect&scope=meeting%3Arecordings_read%20meeting%3Aadmin_schedule_write%20meeting%3Aschedules_read%20meeting%3Aparticipants_read%20meeting%3Aadmin_participants_read%20meeting%3Apreferences_write%20meeting%3Arecordings_write%20meeting%3Apreferences_read%20meeting%3Aadmin_recordings_read%20meeting%3Aschedules_write%20spark%3Akms%20meeting%3Acontrols_write%20meeting%3Aadmin_recordings_write%20meeting%3Acontrols_read%20meeting%3Aparticipants_write%20meeting%3Aadmin_schedule_read&state=set_state_here',
+        authorizationString: config.authUrl,
         clientType: 'confidential'
       }
     }
--- functions/refresh-webex-token/refresh-webex-token.js
+++ functions/refresh-webex-token/refresh-webex-token.js
@@ -1,4 +1,5 @@
 const { save } = require("../save-webex-token/redis-connection");
+const config = require('../get-webex-token/server-config')
 
 // this will be called from CRON
 exports.handler = async (event) => {
@@ -15,8 +16,8 @@ exports.handler = async (event) => {
     },
     config: {
       credentials: {
-        authorizationString: "https://webexapis.com/v1/authorize?client_id=C6139709e0ad95b4d3a6027696af489a2047ed63de929939756a6ce6d8fa5db3c&response_type=code&redirect_uri=https%3A%2F%2Ftwilio-playground.netlify.app%2Fwebex%2Foauth%2Fredirect&scope=meeting%3Arecordings_read%20meeting%3Aadmin_schedule_write%20meeting%3Aschedules_read%20meeting%3Aparticipants_read%20meeting%3Aadmin_participants_read%20meeting%3Apreferences_write%20meeting%3Arecordings_write%20meeting%3Apreferences_read%20meeting%3Aadmin_recordings_read%20meeting%3Aschedules_write%20spark%3Akms%20meeting%3Acontrols_write%20meeting%3Aadmin_recordings_write%20meeting%3Acontrols_read%20meeting%3Aparticipants_write%20meeting%3Aadmin_schedule_read&state=set_state_here",
-        client_secret: "b972772f4d16b0beff8f3bcc156212af67713f04c4281bea5e032ff1f4d78a9f"
+        authorizationString: config.authUrl,
+        client_secret: config.clientSecret
       }
     }
   });
