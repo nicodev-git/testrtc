a3fdb4e19e67ca0e4a925c746278541501a4d5a3
nicodev-git <schaefernicker@gmail.com>
2021-01-15 01:08:04 +0530

    Trying webex oauth 4

--- functions/get-webex-token/get-webex-token.js
+++ functions/get-webex-token/get-webex-token.js
@@ -4,29 +4,10 @@ const assert = require(`assert`);
 const config = require('./server-config.js')
 
 exports.handler = async (event) => {
-  //const token = await get();
-  const code = event.queryStringParameters.code;
-
-  console.log(`get-webex-token:handler`);
-
-  console.log(config.authUrl)
-  assert(code);
-  var webex = Webex.init({
-    config: {
-      credentials: {
-        authorizationString: config.authUrl,
-        clientType: 'confidential'
-      }
-    }
-  });
-  webex.authorization.requestAuthorizationCodeGrant({code})
-    .then(() => {
-
-      res.redirect('/#' + querystring.stringify(webex.credentials.supertoken.toJSON())).end();
-    });
-
-  // return {
-  //   statusCode: 200,
-  //   body: token || "",
-  // };
+  const token = await get();
+  
+  return {
+    statusCode: 200,
+    body: token || "",
+  };
 };
---
+++ functions/start-webex-login/start-webex-login.js
@@ -0,0 +1,32 @@
+const { get } = require("../save-webex-token/redis-connection");
+var Webex = require('webex');
+const assert = require(`assert`);
+const config = require('../get-webex-token/server-config.js')
+
+exports.handler = async (event) => {
+  //const token = await get();
+  const code = event.queryStringParameters.code;
+
+  console.log(`start-webex-login:handler`);
+
+  console.log(config.authUrl)
+  assert(code);
+  var webex = Webex.init({
+    config: {
+      credentials: {
+        authorizationString: config.authUrl,
+        clientType: 'confidential'
+      }
+    }
+  });
+  webex.authorization.requestAuthorizationCodeGrant({code})
+    .then(() => {
+      await save(webex.credentials.supertoken.toJSON());
+      res.redirect('/webex-login.html' + querystring.stringify(webex.credentials.supertoken.toJSON())).end();
+    });
+
+  // return {
+  //   statusCode: 200,
+  //   body: token || "",
+  // };
+};
