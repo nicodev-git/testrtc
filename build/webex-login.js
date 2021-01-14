65d6f33b603aa11d512c89118fac3f19438c76cc
nicodev-git <schaefernicker@gmail.com>
2021-01-14 23:17:02 +0530

    Attempting Webex

--- build/webex-login.js
+++ build/webex-login.js
@@ -1,14 +1,20 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-
 async function saveToken() {
-    const token = "Sample";
-    console.log(`webex-login:saveToken`, {token});
+  const token = "Sample";
+  const url = "/.netlify/functions/save-webex-token";
+  fetch(`${url}/?token=${token}`)
+    .then((answer) => {
+      console.log(`webex-login:saveToken`, { token, answer });
+    })
+    .catch(console.error);
 }
 
 function start() {
-    document.getElementById("save-token").onclick = function() {saveToken()};
-    console.log(`webex-login:start`);
-};
+  document.getElementById("save-token").onclick = function () {
+    saveToken();
+  };
+  console.log(`webex-login:start`);
+}
 
 start();
 
--- src/webex-login.js
+++ src/webex-login.js
@@ -1,3 +1,4 @@
+var Webex = require('webex');
 async function saveToken() {
   const token = "Sample";
   const url = "/.netlify/functions/save-webex-token";
@@ -13,6 +14,33 @@ function start() {
     saveToken();
   };
   console.log(`webex-login:start`);
+
+  var webex = Webex.init({
+    config: {
+      credentials: {
+        authorizationString: '',
+        clientType: 'confidential',
+        refreshCallback: function(webex, token) {
+          return fetch(`/webex/oauth/refresh`, {
+            method: `POST`,
+            body: JSON.stringify({
+              refresh_token: token.refresh_token
+            })
+          })
+            .then((res) => res.json())
+        }
+      }
+    }
+  });
+
+  webex.once(`ready`, function () {
+    if (webex.canAuthorize) {
+      // your app logic goes here
+    }
+    else {
+      webex.authorization.initiateLogin()
+    }
+  });
 }
 
 start();
