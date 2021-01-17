55bbfbc4658cf7bb71c922066818ba7cd1c46053
nicodev-git <schaefernicker@gmail.com>
2021-01-17 19:40:35 +0530

    webex oauth 22 trying http

--- functions/get-webex-token/server-config.js
+++ functions/get-webex-token/server-config.js
@@ -1,4 +1,6 @@
 const authUrl = 'https://webexapis.com/v1/authorize?client_id=C6139709e0ad95b4d3a6027696af489a2047ed63de929939756a6ce6d8fa5db3c&response_type=code&redirect_uri=https%3A%2F%2Ftwilio-playground.netlify.app%2F.netlify%2Ffunctions%2Fstart-webex-redirect&scope=meeting%3Arecordings_read%20spark%3Aall%20meeting%3Aschedules_read%20identity%3Aplaceonetimepassword_create%20meeting%3Aparticipants_read%20meeting%3Apreferences_write%20meeting%3Arecordings_write%20meeting%3Apreferences_read%20meeting%3Aschedules_write%20spark%3Akms%20meeting%3Acontrols_write%20meeting%3Acontrols_read%20meeting%3Aparticipants_write&state=qualityrtc';
+const clientId = 'C6139709e0ad95b4d3a6027696af489a2047ed63de929939756a6ce6d8fa5db3c'
 const clientSecret = '2f8b464de0b1ed5f2c025b0b322fa4b28059c7c63ca99d43e49ef0799327b09f'
+const redirectUri = 'https://twilio-playground.netlify.app/.netlify/functions/start-webex-redirect'
 
-module.exports = { authUrl, clientSecret }
+module.exports = { authUrl, clientId, clientSecret, redirectUri }
--- functions/start-webex-redirect/start-webex-redirect.js
+++ functions/start-webex-redirect/start-webex-redirect.js
@@ -1,27 +1,63 @@
 const { get } = require("../save-webex-token/redis-connection");
 var Webex = require('webex');
 const assert = require(`assert`);
+const axios = require('axios')
+var querystring = require('querystring');
 const config = require('../get-webex-token/server-config.js')
 
 exports.handler = async (event) => {
   const code = event.queryStringParameters.code;
 
-  console.log(`start-webex-login:handler`);
+  console.log(`start-webex-redirect:handler`);
 
   console.log(config.authUrl)
   assert(code);
-  var webex = Webex.init({
-    config: {
-      credentials: {
-        authorizationString: config.authUrl,
-        client_secret: config.clientSecret
-      }
-    }
-  });
+  // var webex = Webex.init({
+  //   config: {
+  //     credentials: {
+  //       authorizationString: config.authUrl,
+  //       client_secret: config.clientSecret
+  //     }
+  //   }
+  // });
 
-  await webex.once(`ready`)
-  return await webex.authorization.requestAuthorizationCodeGrant(event.queryStringParameters)
-    .then(() => {
+  // await webex.once(`ready`)
+  // return await webex.authorization.requestAuthorizationCodeGrant({code, grant_type:'authorization_code'})
+  //   .then(() => {
+  //     return {
+  //       statusCode: 302,
+  //       body: "",
+  //       headers: {
+  //         location: "/webex-login.html"
+  //       }
+  //     }
+  //   })
+  //   .catch((err) => {
+  //     return {
+  //       statusCode: 500,
+  //       body: JSON.stringify({ err }),
+  //       headers: {
+  //         "x-code": code
+  //       }
+  //     }
+  //   });
+
+  return axios
+    .post('/https://webexapis.com/v1/access_token',
+      querystring.stringify({
+        grant_type: 'authorization_code',
+        client_id: config.clientId,
+        client_secret: config.clientSecret,
+        code,
+        redirect_uri: config.redirectUri
+      }), {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    })
+    .then((res) => {
+      console.log(`statusCode: ${res.statusCode}`)
+      console.log(res)
       return {
         statusCode: 302,
         body: "",
@@ -30,13 +66,11 @@ exports.handler = async (event) => {
         }
       }
     })
-    .catch((err) => {
+    .catch((error) => {
+      console.error(error)
       return {
         statusCode: 500,
-        body: JSON.stringify({ err }),
-        headers: {
-          "x-code": code
-        }
+        body: JSON.stringify({ error })
       }
-    });
+    })
 };
