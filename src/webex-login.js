e5af3ac333cb4faf6a6536cb2efe88d6c3af15d7
nicodev-git <schaefernicker@gmail.com>
2021-01-20 12:31:37 +0530

    relative path for login redirection.

--- src/webex-login.js
+++ src/webex-login.js
@@ -1,12 +1,9 @@
 function start() {
   document.getElementById("user1-login").onclick = function () {
-    window.location.replace("https://twilio-playground.netlify.app/.netlify/functions/start-webex-login?user=user1");
+    window.location.replace("./.netlify/functions/start-webex-login?user=user1");
   };
   document.getElementById("user2-login").onclick = function () {
-    const loginUri = process.env.WEBEX_LOGIN_URI || "";
-    loginUri+= "?user=user2"
-    console.log(loginUri);
-    window.location.replace("https://twilio-playground.netlify.app/.netlify/functions/start-webex-login?user=user2");
+    window.location.replace("./.netlify/functions/start-webex-login?user=user2");
   };
   console.log(`webex-login:start`);
 }
