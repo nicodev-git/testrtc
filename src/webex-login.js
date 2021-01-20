ffacd1977cd5310c0db5490f53faf1631a69b682
nicodev-git <schaefernicker@gmail.com>
2021-01-20 11:59:52 +0530

    trying env in browser side.

--- src/webex-login.js
+++ src/webex-login.js
@@ -3,6 +3,9 @@ function start() {
     window.location.replace("https://twilio-playground.netlify.app/.netlify/functions/start-webex-login?user=user1");
   };
   document.getElementById("user2-login").onclick = function () {
+    const loginUri = process.env.WEBEX_LOGIN_URI || "";
+    loginUri+= "?user=user2"
+    console.log(loginUri);
     window.location.replace("https://twilio-playground.netlify.app/.netlify/functions/start-webex-login?user=user2");
   };
   console.log(`webex-login:start`);
